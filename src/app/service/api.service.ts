98c4a140aba3ff24531e4ef4fab7b4703c0cad32
Tushar Kamthe <tykamthe@gmail.com>
2023-01-29 20:43:16 +0530

    submitting project

---
+++ src/app/service/api.service.ts
@@ -0,0 +1,48 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import {map} from 'rxjs/operators'
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+baseURL ="http://localhost:3000/Users"
+
+  constructor(private http : HttpClient) { }
+
+  getRecords(){
+   return this.http.get(this.baseURL)
+  }
+  // GET Request ('R' in the CRUD)
+
+  getRecord(){
+    return this.http.get<any>("http://localhost:3000/Items")
+    .pipe(map((res:any)=>{
+      return res;
+    }))
+  }
+// getRecord(id:any){
+//   let getURL = `${this.baseURL}/${id}`
+//   return this.http.get(getURL)
+// }
+
+
+  // DELETE Request ('D' in CRUD)
+deleteRecord(id:any){
+  let deleteURL = `${this.baseURL}/${id}`
+  return this.http.delete(deleteURL)
+}
+
+//POST Request ('C' in the CRUD)
+addRecord(empData:any){
+  return this.http.post(this.baseURL, empData)
+}
+
+//PUT Request  (U in the CRUD)
+updateRecord(empData: any){
+  let putURL = `${this.baseURL}/${empData.id}`
+  return this.http.put(putURL, empData)
+}
+}
+// https://fakestoreapi.com/products/
+// \src\shared\api
---
+++ src/app/service/global.service.ts
@@ -0,0 +1,54 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { map } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GlobalService {
+  navigate(arg0: string[]) {
+    throw new Error('Method not implemented.');
+  }
+
+  baseURL:string =" http://localhost:3000"
+  constructor(private httpClient:HttpClient) { }
+  
+  getRecords(path:string){
+    const url = `${this.baseURL}/${path}`
+    return this.httpClient.get(url)
+  }
+   // GET Request ('R' in the CRUD)
+   getRecord(path:string, id:any){
+    const url = `${this.baseURL}/${path}/${id}`
+    return this.httpClient.get<any>(url)
+    .pipe(map((res:any)=>{
+      return res;
+    }))
+  }
+  //DELETE Request ('D' in the CRUD)
+  deleteRecords(path:string, id:any){
+    const url = `${this.baseURL}/${path}/${id}`
+    return this.httpClient.delete(url)
+  }
+
+  //Post Request ('C' in the CRUD)
+  addRecord(path:string, empData:any){
+    const url = `${this.baseURL}/${path}`
+    return this.httpClient.post(url, empData)
+  }
+  
+  //PUT Request  (U in the CRUD)
+updateRecord(path:string, empData: any){
+  const url = `${this.baseURL}/${path}/${empData.id}`
+  return this.httpClient.put(url, empData)
+}
+
+
+signIn(user:any){
+  sessionStorage.setItem("userkey", user)
+}
+
+signOut(){
+  sessionStorage.removeItem("userkey")
+}
+}
